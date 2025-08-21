b08d5cd89f6b5180f93d60c8d03c3a28d4b63b9d
marcusmcb <marcusmcb@gmail.com>
2025-08-21 14:18:21 -0700

    added fall 2025 schedule to site

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import './App.css'
+import { useState } from 'react'
+import { events } from './data/events'
 
 const App = () => {
+	const [page, setPage] = useState<'main' | 'events'>('main')
 	const profileImg = '/images/jack_soccer.jpg'
 	const links = [
+		{
+			href: '#events',
+			label: 'Upcoming Events',
+			onClick: (e: React.MouseEvent) => {
+				e.preventDefault()
+				setPage('events')
+			},
+		},
 		{
 			href: 'https://recruit-match.ncsasports.org/clientrms/athletes/11904466',
 			label: 'NCSA Profile',
 		},
 		{
-			href: 'https://vimeo.com/1105218858',
-			label: 'Assist On Goal',
-		},
-		{
 			href: 'https://vimeo.com/1105215223',
 			label: 'Highlight Reel',
 		},
+		// {
+		// 	href: 'https://vimeo.com/1105218858',
+		// 	label: 'Assist On Goal',
+		// },
+
 		{ href: 'https://www.nhhsboyssoccer.com/', label: 'NHHS Sailors' },
 		{
 			href: 'https://www.pateadores.org/newport-beach/costa-mesa#NBCMTEAMS',
@@ -28,25 +40,65 @@ const App = () => {
 
 	return (
 		<div className='container'>
-			<img src={profileImg} alt='Profile' className='profile-img' />
-			<div className='profile-info'>
-				<div className='profile-name'>Jack McBride</div>
-				<div className='profile-school'>Newport Harbor High School</div>
-				<div className='profile-gpa'>Class of 2026 - 3.8 GPA</div>
-			</div>
-			<div className='links-col'>
-				{links.map((link, idx) => (
-					<a
-						key={idx}
-						href={link.href}
+			{page === 'main' && (
+				<>
+					<img src={profileImg} alt='Profile' className='profile-img' />
+					<div className='profile-info'>
+						<h1 className='profile-name'>Jack McBride</h1>
+						<div className='profile-school'>Newport Harbor High School</div>
+						<div className='profile-gpa'>Class of 2026 - 3.8 GPA</div>
+					</div>
+					<div className='links-col'>
+						{links.map((link, idx) => (
+							<a
+								key={idx}
+								href={link.href}
+								className='profile-link'
+								target={link.href.startsWith('mailto:') ? undefined : '_blank'}
+								rel='noopener noreferrer'
+								onClick={link.onClick}
+							>
+								{link.label}
+							</a>
+						))}
+					</div>
+				</>
+			)}
+			{page === 'events' && (
+				<div className='events-page'>
+					<h2 className='events-title'>Upcoming Soccer Events</h2>
+					<div className='events-list'>
+						{events.map((event, idx) => (
+							<div className='event-card' key={idx}>
+								<div className='event-name'>{event.name}</div>
+								<div className='event-details'>
+									<span>{event.date}</span> | <span>{event.time}</span>
+								</div>
+								<div className='event-location'>
+									<span>{event.location}</span>
+									{event.map && (
+										<a
+											href={event.map}
+											className='event-map-link'
+											target='_blank'
+											rel='noopener noreferrer'
+										>
+											&nbsp;View Map
+										</a>
+									)}
+								</div>
+							</div>
+						))}
+					</div>
+					<button
 						className='profile-link'
-						target='_blank'
-						rel='noopener noreferrer'
+						style={{ marginTop: '2rem' }}
+						onClick={() => setPage('main')}
 					>
-						{link.label}
-					</a>
-				))}
-			</div>
+						Back to Profile
+					</button>
+				</div>
+			)}
 		</div>
 	)
 }
---
+++ src/data/events.ts
@@ -0,0 +1,44 @@
+export const events = [
+	{
+		name: 'Pateadores vs Fullerton City FC',
+		date: '2025-08-23',
+		time: '10:40 AM',
+		location: 'Hicks Canyon Community Park',
+		map: 'https://www.google.com/maps/search/?api=1&query=3864%20Viewpark%20Ave%2C%20Irvine%2C%20CA%2092602%2C%20USA',
+	},
+	{
+		name: 'Pateadores vs LA Surf Pasadena',
+		date: '2025-08-23',
+		time: '2:40 PM',
+		location: 'Hicks Canyon Community Park',
+		map: 'https://www.google.com/maps/search/?api=1&query=3864%20Viewpark%20Ave%2C%20Irvine%2C%20CA%2092602%2C%20USA',
+	},
+	{
+		name: 'Pateadores vs Zeta Academy',
+		date: '2025-08-24',
+		time: '9:20 AM',
+		location: 'Great Park Stadium',
+		map: 'https://www.google.com/maps/search/?api=1&query=8000%20Great%20Park%20blvd%20Irvine%2C%20CA%2092618',
+	},
+	{
+		name: 'Pateadores vs Fire FC',
+		date: '2025-08-30',
+		time: '3:30 PM',
+		location: 'Great Park Stadium',
+		map: 'https://www.google.com/maps/search/?api=1&query=8000%20Great%20Park%20blvd%20Irvine%2C%20CA%2092618',
+	},
+	{
+		name: 'Pateadores vs Ballistic United',
+		date: '2025-08-30',
+		time: '8:00 PM',
+		location: 'Great Park Stadium',
+		map: 'https://www.google.com/maps/search/?api=1&query=8000%20Great%20Park%20blvd%20Irvine%2C%20CA%2092618',
+	},
+	{
+		name: 'Pateadores vs California Rush',
+		date: '2025-08-31',
+		time: '12:30 PM',
+		location: 'Great Park Stadium',
+		map: 'https://www.google.com/maps/search/?api=1&query=8000%20Great%20Park%20blvd%20Irvine%2C%20CA%2092618',
+	},
+]
