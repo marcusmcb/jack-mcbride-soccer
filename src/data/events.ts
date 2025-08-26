b0c977bae072e0d06ff19f7e7a4a29a9ad32dac3
marcusmcb <marcusmcb@gmail.com>
2025-08-26 11:38:20 -0700

    updated events

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,25 +1,25 @@
 export const events = [
-	{
-		name: 'Pateadores vs Fullerton City FC',
-		date: '2025-08-23',
-		time: '10:40 AM',
-		location: 'Hicks Canyon Community Park',
-		map: 'https://www.google.com/maps/search/?api=1&query=3864%20Viewpark%20Ave%2C%20Irvine%2C%20CA%2092602%2C%20USA',
-	},
-	{
-		name: 'Pateadores vs LA Surf Pasadena',
-		date: '08/23/2025',
-		time: '2:40 PM',
-		location: 'Hicks Canyon Community Park',
-		map: 'https://www.google.com/maps/search/?api=1&query=3864%20Viewpark%20Ave%2C%20Irvine%2C%20CA%2092602%2C%20USA',
-	},
-	{
-		name: 'Pateadores vs Zeta Academy',
-		date: '08/24/2025',
-		time: '9:20 AM',
-		location: 'Great Park Stadium',
-		map: 'https://www.google.com/maps/search/?api=1&query=8000%20Great%20Park%20blvd%20Irvine%2C%20CA%2092618',
-	},
+	// {
+	// 	name: 'Pateadores vs Fullerton City FC',
+	// 	date: '2025-08-23',
+	// 	time: '10:40 AM',
+	// 	location: 'Hicks Canyon Community Park',
+	// 	map: 'https://www.google.com/maps/search/?api=1&query=3864%20Viewpark%20Ave%2C%20Irvine%2C%20CA%2092602%2C%20USA',
+	// },
+	// {
+	// 	name: 'Pateadores vs LA Surf Pasadena',
+	// 	date: '08/23/2025',
+	// 	time: '2:40 PM',
+	// 	location: 'Hicks Canyon Community Park',
+	// 	map: 'https://www.google.com/maps/search/?api=1&query=3864%20Viewpark%20Ave%2C%20Irvine%2C%20CA%2092602%2C%20USA',
+	// },
+	// {
+	// 	name: 'Pateadores vs Zeta Academy',
+	// 	date: '08/24/2025',
+	// 	time: '9:20 AM',
+	// 	location: 'Great Park Stadium',
+	// 	map: 'https://www.google.com/maps/search/?api=1&query=8000%20Great%20Park%20blvd%20Irvine%2C%20CA%2092618',
+	// },
 	{
 		name: 'Pateadores vs Fire FC',
 		date: '08/30/2025',
